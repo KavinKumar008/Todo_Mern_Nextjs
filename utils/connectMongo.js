c51ffdca5338a39fdacf0437dc126d065bf0ebbe
KavinKumar008 <kavikumarsraja@gmail.com>
2025-11-05 12:58:58 +0530

    updated local

--- utils/connectMongo.js
+++ utils/connectMongo.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectMongo = async () => {
   try {
-    mongoose.connect(process.env.MONGO_URI);
+    mongoose.connect(process.env.MONGODB_URI);
     console.log("MongoDb Connected");
   } catch (error) {
     console.log(error, "Failed to connect");
